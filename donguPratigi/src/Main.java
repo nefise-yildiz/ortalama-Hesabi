ca16926ec7c4ffa1a9c5c3fe73316220cea2b0e5
nefise-yildiz <nefiseyildiz07@gmail.com>
2024-02-14 14:55:16 +0300

    düzenlendi

--- donguPratigi/src/Main.java
+++ donguPratigi/src/Main.java
@@ -7,11 +7,19 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.print("Bir Sayı Giriniz:");
         k = input.nextInt();
+        int toplam=0,sayac=0;
         for (int i = 0; i <= k; i++) {
             if (i % 3 == 0 && i % 4 == 0) {
-                System.out.println(i);
+                sayac++;
+                toplam+=i;
+                System.out.println("3 ve 4' e bölünen sayılar : " + i);
             }
+
         }
+        System.out.println("-----------------------");
+        System.out.println("Sayac = " + sayac);
+        System.out.println("Toplam = " + toplam);
+        System.out.println("3 ve 4' e bölünen sayıların ortalaması = " + toplam/sayac);
 
     }
 }
